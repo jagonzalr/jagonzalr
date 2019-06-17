5203819ee3389ad96ef3ab7336baee8704336165
Jose Antonio Gonzalez <jagonzalr@gmail.com>
2019-06-17 16:32:06 +0200

    Update tech stack

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -7,17 +7,10 @@ export default () => (
     <ul>
       <li>
         <span>
-          <a href='https://nodejs.org/' target='blank'>
-            Node.js
-          </a>
-        </span>
-        ,&nbsp;
-        <span>
           <a href='https://reactjs.org/' target='blank'>
             React
           </a>
-        </span>
-        ,&nbsp;
+        </span>,&nbsp;
         <span>
           <a href='https://redux.js.org/' target='blank'>
             Redux
@@ -32,6 +25,11 @@ export default () => (
       </li>
       <li>
         <span>
+          <a href='https://nodejs.org/' target='blank'>
+            Node.js
+          </a>
+        </span>,&nbsp;
+        <span>
           <a href='https://graphql.org/' target='blank'>
             GraphQL
           </a>
@@ -43,7 +41,9 @@ export default () => (
         </span>&nbsp;
       </li>
       <li>
-        <span><a href='https://www.djangoproject.com/' target='blank'>Django</a></span>,&nbsp;
+        <span>
+          <a href='https://www.djangoproject.com/' target='blank'>Django</a>
+        </span>,&nbsp;
         <span>
           <a href='http://www.django-rest-framework.org/' target='blank'>
             DRF
@@ -84,6 +84,11 @@ export default () => (
           <a href='https://circleci.com/' target='blank'>
             Circle CI
           </a>
+        </span>&nbsp;and&nbsp;
+        <span>
+          <a href='https://www.netlify.com/' target='blank'>
+            Netlify
+          </a>
         </span>
       </li>
       <li>
@@ -95,7 +100,7 @@ export default () => (
         (Amplify, Lambda, SNS, S3,
       </li>
       <li>
-        CloudFront, SQS, SES, EB,
+        CloudFront, SQS, SES, EB, Cognito,
       </li>
       <li>
         Elastic Transcoder, Route53)
