d8e1dc526adaa1322233d0cb61b89b4824294ad0
Jose Antonio Gonzalez <jagonzalr@gmail.com>
2020-04-28 16:44:51 +0200

    Move runtime to layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import '../styles/index.scss'
 
+require('regenerator-runtime/runtime')
+
 export default ({ children }) => (
 	<div className='container mt-2'>
 		{children}
