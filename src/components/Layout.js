ae050b50d3e638b57edb91405732b4e2fae604df
Jose Antonio Gonzalez <jagonzalr@gmail.com>
2020-04-29 08:06:41 +0200

    Use latest version of useTyping hook

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 import '../styles/index.scss'
 
-require('regenerator-runtime/runtime')
-
 export default ({ children }) => (
 	<div className='container mt-2'>
 		{children}
