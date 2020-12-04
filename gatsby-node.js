012c89df686f488b9be4eed070ea3efe04f680db
José Antonio González <jagonzalr@gmail.com>
2020-12-04 10:23:55 +0200

    Add new posts section and update graphql queries

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,7 +19,7 @@ exports.createPages = async ({ graphql, actions }) => {
 	const { createPage } = actions
   const result = await graphql(`
     {
-      projects: allMdx {
+      projects: allMdx(filter: {frontmatter: {type: {eq: "project"}}}) {
         edges {
           node {
             fields {
