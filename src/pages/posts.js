d7a870942d29518041459ac7abf4969bc26529c2
José Antonio González <jagonzalr@users.noreply.github.com>
2020-12-27 09:45:43 +0200

    Add SQS post (#8)

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -10,7 +10,8 @@ export default ({ data, location }) => (
     		<h2 className='d-inline subtitle underline'>Posts</h2>
       </div>
   		{data.allMdx.edges.map(({ node }) => (
-        <div key={node.id} className='col-sm-12 my-1'>
+        <div key={node.id} className='col-sm-12 mb-2 mt-1'>
+          <small>{new Date(node.frontmatter.date).toDateString()}</small>
         	<a href={node.frontmatter.link} target='blank'>
   	        <h4>{node.frontmatter.title}</h4>
           </a>
@@ -23,7 +24,7 @@ export default ({ data, location }) => (
 
 export const query = graphql`
   query {
-    allMdx(sort: {fields: frontmatter___title, order: ASC}, filter: { frontmatter: { type: { eq: "post" } } }) {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}, filter: { frontmatter: { type: { eq: "post" } } }) {
       totalCount
       edges {
         node {
@@ -31,6 +32,7 @@ export const query = graphql`
           frontmatter {
             title
             tagline
+            date
             link
           }
           fields {
