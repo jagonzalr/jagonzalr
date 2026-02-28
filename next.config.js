170dd4b333decb2aa4d7692021340445e4afff3d
Jose Antonio Gonzalez <jagonzalr@gmail.com>
2026-02-28 18:02:13 +0000

    Remove image optimization

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@ const nextConfig = {
   i18n: {
     locales: ['en'],
     defaultLocale: 'en'
-  }
+  },
+  images: {
+    unoptimized: true
+  },
+  output: 'export'
 }
 
 module.exports = nextConfig
